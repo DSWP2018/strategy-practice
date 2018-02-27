b909e9d33ebfa90e88650e57da0e12bf868aefd4
adriana zarate zarate <adrianazarate0397@gmail.com>
2018-02-26 19:57:00 -0600

    Pokemon Practice [02]

---
+++ src/main/java/com/iteso/nintendo/Ditto.java
@@ -0,0 +1,85 @@
+package com.iteso.nintendo;
+
+public class Ditto extends PokemonCharacter {
+    /**
+     * Hit Points are the maximum life of pokemon.
+     */
+    public static final int HIT_POINTS = 77;
+    /**
+     * Defense multiplier value between 0-1.
+     */
+    public static final double DEFENSE_MULTIPLIER = 0.3;
+    /**
+     * Damage from 1 - 20.
+     */
+    public static final int MAIN_ATTACK_DAMAGE = 3;
+    /**
+     * Damage from 1 -25.
+     */
+    public static final int SECOND_ATTACK_DAMAGE = 17;
+
+    public FireBallAttack fire;
+    public PoisonAttack poison;
+
+    /**
+     * Pikachu constructor.
+     */
+    public Ditto() {
+        setType("fire");
+        setName("Ditto");
+        setHasEvolution(true);
+        setSecondAttack(poison);
+        setMainAttack(fire);
+        setHitPoints(HIT_POINTS);
+        setDefenseMultiplier(DEFENSE_MULTIPLIER);
+        setMainAttackDamage(MAIN_ATTACK_DAMAGE);
+        setSecondAttackDamage(SECOND_ATTACK_DAMAGE);
+    }
+
+    @Override
+    public final String evolve() {
+        return null;
+    }
+
+    @Override
+    public final String defend(final int attack) {
+        int damage;
+
+        damage = (int) (attack * getDefenseMultiplier());
+        int newHP = getHitPoints() - damage;
+
+        String defendMessage = new String("Defending attack, damage caused is "
+                + damage + " new HP is " + newHP);
+
+        setHitPoints(newHP);
+        return defendMessage;
+
+    }
+
+    @Override
+    public final String secondAttack() {
+
+        String attackMessage = new String("Attacking opponent with "
+                + getSecondAttack()
+                + " causing a damage of " + getSecondAttackDamage());
+        return attackMessage;
+
+
+    }
+
+    @Override
+    public final String mainAttack() {
+        String attackMessage = new String("Attacking opponent with "
+                + getMainAttack()
+                + " causing a damage of " + getMainAttackDamage());
+        return attackMessage;
+
+    }
+
+    @Override
+    public final void setNewAttack(final int attack, final int attackDamage,
+                                   final String newAttack) {
+    }
+
+
+}
