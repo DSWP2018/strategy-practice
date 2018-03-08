d133072b2898ca2dfa5732673588125df218dfb6
Frank <javier.francisco01.09@gmail.com>
2018-03-07 22:54:00 -0600

    empaquetados test pokemons

---
+++ src/test/java/com/iteso/nintendo/pokemonesTest/MudkipTest.java
@@ -0,0 +1,47 @@
+package com.iteso.nintendo.pokemonesTest;
+import com.iteso.nintendo.PokemonCharacter;
+import com.iteso.nintendo.ataques.AtaqueRapido;
+import com.iteso.nintendo.ataques.Attacks;
+import com.iteso.nintendo.tipos.PokemonType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+
+public class MudkipTest {
+    Attacks quickAttack;
+    PokemonCharacter mudkip;
+    PokemonType pokemonType;
+    PokemonType enemyType;
+
+    @Before
+    public void setUp(){
+        mudkip = mock(PokemonCharacter.class, Mockito.CALLS_REAL_METHODS);
+        pokemonType = mock(PokemonType.class);
+        enemyType = mock(PokemonType.class);
+        quickAttack = new AtaqueRapido();
+    }
+
+    @Test
+    public void testAttackName(){
+        Assert.assertEquals("Quick Attack", quickAttack.attackName());
+    }
+
+    @Test
+    public void testAttackType(){
+        Assert.assertEquals("Normal", quickAttack.attackType());
+    }
+
+    @Test
+    public void testAttackDamage(){
+        Assert.assertEquals(40, quickAttack.attackDamage());
+    }
+
+    @Test
+    public void testAttackOpponent(){
+        mudkip.setType(pokemonType.type());
+        Assert.assertEquals("Attacking opponent with Quick Attack damage: 40", quickAttack.attackOpponent()+" dmage: "+quickAttack.attackDamage());
+    }
+}
---
+++ src/test/java/com/iteso/nintendo/pokemonesTest/SquirtleTest.java
@@ -0,0 +1,47 @@
+package com.iteso.nintendo.pokemonesTest;
+import com.iteso.nintendo.PokemonCharacter;
+import com.iteso.nintendo.ataques.AtaqueRapido;
+import com.iteso.nintendo.ataques.Attacks;
+import com.iteso.nintendo.tipos.PokemonType;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.mock;
+
+public class SquirtleTest {
+    Attacks quickAttack;
+    PokemonCharacter squirtle;
+    PokemonType pokemonType;
+    PokemonType enemyType;
+
+    @Before
+    public void setUp(){
+        squirtle = mock(PokemonCharacter.class, Mockito.CALLS_REAL_METHODS);
+        pokemonType = mock(PokemonType.class);
+        enemyType = mock(PokemonType.class);
+        quickAttack = new AtaqueRapido();
+    }
+
+    @Test
+    public void testAttackName(){
+        Assert.assertEquals("Quick Attack", quickAttack.attackName());
+    }
+
+    @Test
+    public void testAttackType(){
+        Assert.assertEquals("Normal", quickAttack.attackType());
+    }
+
+    @Test
+    public void testAttackDamage(){
+        Assert.assertEquals(40, quickAttack.attackDamage());
+    }
+
+    @Test
+    public void testAttackOpponent(){
+        squirtle.setType(pokemonType.type());
+        Assert.assertEquals("Attacking opponent with Quick Attack damage: 40", quickAttack.attackOpponent()+" dmage: "+quickAttack.attackDamage());
+    }
+}
